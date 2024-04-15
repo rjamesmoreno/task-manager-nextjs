54f4cad959ba8acefab74faa5749c8df9f36892f
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-15 10:01:51 +0800

    feat:  created custom hook for fetching data

---
+++ src/app/components/useFetch.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const useFetch = (url: string | URL | Request) => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  return {data};
+};
+
+export default useFetch;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,11 @@
-'use client'
+"use client";
 import { useEffect, useState } from "react";
 import CreateTask from "./components/CreateTask";
 import TaskList from "./components/TaskList";
+import useFetch from "./components/useFetch";
 
 export default function Home() {
-  const [tasks, setTasks] = useState(null);
-
-
-  useEffect(() => {
-    fetch("http://localhost:8000/tasks").then((res) => {
-      return res.json();
-    })
-    .then(data => {
-      console.log(data);
-      setTasks(data);
-    })
-    .catch(err => {
-      console.log(err.message);
-    })
-  }, []);
-
+  const {data: tasks} = useFetch('http://localhost:8000/tasks')
   return (
     <main className="flex items-center justify-center h-screen bg-[#23366D]">
       <div className="h-[80vh] w-[70vw] bg-[#3450A1] rounded-xl">
