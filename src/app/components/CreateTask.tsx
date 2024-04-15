f4991f8f6f8bc5e23de3eeaf62d9bb5931039f48
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-15 12:08:15 +0800

    feat: made create task functionable

--- src/app/components/CreateTask.tsx
+++ src/app/components/CreateTask.tsx
@@ -1,30 +1,41 @@
-"use client";
-import { useState } from "react";
+'use client'
+import { FormEventHandler, useState } from "react";
+let nextId=0;
 
 export default function CreateTask() {
-  let nextId=0;
-  const [taskName, setTaskName] = useState('');
-  const [tasks, setTasks] = useState([])
+  const [taskName, setTaskName] = useState("");
 
-  const handleCreateTask = () => {
-    
+  const handleCreateTask: FormEventHandler<HTMLFormElement> = (e) => { 
+    e.preventDefault();
+    const task = { taskName };
+    fetch("http://localhost:8000/tasks", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(task),
+    }).then(() => {
+      console.log("task added successfully");
+    });
+    setTaskName('');
   };
 
   return (
     <div className="relative p-4">
-      <input
-        className="py-2 px-4 w-[30rem] rounded-lg bg-[#183075] text-white"
-        placeholder="Enter Task"
-        type="text"
-        value={taskName}
-        onChange={(e) => setTaskName(e.target.value)}
-      ></input>
-      <button
-        onClick={handleCreateTask}
-        className="btn bg-[#EB06FF] active:bg-[#C617D5] active:text-white/75 text-white text-xl font-black text-center rounded-[100%] size-8 absolute ml-[-35px] top-5"
-      >
-        +
-      </button>
+      <form onSubmit={handleCreateTask}>
+        <input
+          className="py-2 px-4 w-full rounded-lg bg-[#183075] text-white"
+          placeholder="Enter Task"
+          type="text"
+          required
+          value={taskName}
+          onChange={(e) => setTaskName(e.target.value)}
+        ></input>
+        <button
+          type="submit"
+          className="btn bg-[#EB06FF] active:bg-[#C617D5] active:text-white/75 text-white text-xl font-black text-center rounded-[100%] size-8 absolute ml-[-35px] top-5"
+        >
+          +
+        </button>
+      </form>
     </div>
   );
 }
