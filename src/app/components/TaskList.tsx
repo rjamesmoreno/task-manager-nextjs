cabd8b11f8431103320724f900f591124c025617
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-15 17:02:50 +0800

    feat: added delete task request

--- src/app/components/TaskList.tsx
+++ src/app/components/TaskList.tsx
@@ -6,16 +6,23 @@ interface Task {
 export default function TaskList(props: { tasks: Task[] }) {
   const tasks = props.tasks;
 
+  const deleteTask = async (taskId: number) => {
+  const response = await fetch(`http://localhost:8000/tasks/${taskId}`, {
+    method: 'DELETE',
+  });
+  
+  }
+
   return (
     <div>
       {tasks.map((task) => (
-        <div key={task.id} className="py-2 px-4 mx-2 flex justify-between items-center">
-          <div className="items-center mx-2">
-            <input type="checkbox" />
+        <div key={task.id} className="py-2 px-4 flex">
+          <div className="h-[4vh] w-[2vw] bg-[#23366D] pl-[9px] pt-1 mr-1 rounded-l-lg">
+            <input type="checkbox" className="" />
           </div>
-          <div className="bg-[#23366D] w-[50vh] h-[4vh] rounded-[5px] text-white pl-5 content-center">{task.taskName}</div>
-          <div>
-            <button className="bg-red-500 rounded-[5px] h-[4vh] w-full mx-2 items-center">Delete</button>
+          <div className="bg-[#23366D] w-[24vw] h-[4vh] pl-2 text-white content-center">{task.taskName}</div>
+          <div className="ml-1">
+            <button onClick={() => deleteTask(task.id)} className="bg-red-500 rounded-r-[5px] w-[4vw] h-[4vh]">Delete</button>
           </div>
         </div>
       ))}
