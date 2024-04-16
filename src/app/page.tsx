b6b85cfd1a8d18d18b04730c30f5817e7af556ed
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-16 14:52:40 +0800

    fix: removed page reload ever after state changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,33 @@
 import { useEffect, useState } from "react";
 import CreateTask from "./components/CreateTask";
 import TaskList from "./components/TaskList";
-import useFetch from "./components/useFetch";
 
 export default function Home() {
-  const {data: tasks} = useFetch('http://localhost:8000/tasks')
+  const [taskList, setTaskList] = useState([]);
+  const useFetch = (url: string | URL | Request) => {
+    fetch(url, {cache: 'no-store'})
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setTaskList(data);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+  };
+
+  useEffect(() => {
+      useFetch("http://localhost:8000/tasks")
+  }, [taskList]);
   return (
-    <main className="flex items-center justify-center h-screen bg-[#23366D]">
-      <div className="h-[80vh] w-[70vw] bg-[#3450A1] rounded-xl">
+    <main className="flex items-center justify-center p-16 bg-[#23366D] min-h-screen">
+      <div className="w-[70vw] p-12 bg-[#3450A1] rounded-xl min-h-[80vh]">
         <div className="grid grid-flow-row justify-center">
           <h1 className="text-2xl text-white pt-5 text-center">Task Manager</h1>
           <CreateTask />
           <h2 className=" text-white px-4 text-lg">Tasks To Do</h2>
-          {tasks && <TaskList tasks={tasks} />}
+          {taskList && <TaskList tasks={taskList}/>}
         </div>
       </div>
     </main>
