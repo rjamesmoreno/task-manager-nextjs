52eda38905024bc0cac9fe8c056a67209c99be9d
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-17 17:50:54 +0800

    feat: created api commands

---
+++ api.tsx
@@ -0,0 +1,44 @@
+import { ITask } from "@/app/types/tasks";
+
+const baseUrl = "http://localhost:8000";
+
+export const getAllTasks = async () => {
+  const res = await fetch(`${baseUrl}/tasks`, { cache: "no-store" });
+  const tasks = await res.json();
+  console.log(tasks);
+  return tasks;
+};
+
+export const addTask = async (task: ITask) => {
+  const res = await fetch(`${baseUrl}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  });
+  const newTodo = await res.json();
+  return newTodo;
+};
+
+export const deleteTask = async (id: string) => {
+  await fetch(`${baseUrl}/tasks/${id}`, {
+    method: "DELETE",
+  });
+};
+
+export const FinishTask = async (task: ITask, isFinished: boolean) => {
+  const res = await fetch(`${baseUrl}/tasks/${task.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: task.id,
+      title: task.title,
+      isFinished: isFinished,
+    }),
+  });
+  const updatedTodo = await res.json();
+  return updatedTodo;
+};
