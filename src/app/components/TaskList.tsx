f51dacde32a90076069baf38410c057cad2ef08a
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-17 17:58:39 +0800

    chore: changed padding

--- src/app/components/TaskList.tsx
+++ src/app/components/TaskList.tsx
@@ -17,13 +17,13 @@ export default function TaskList({ tasks }: TaskListProps) {
       <div className=" my-2 h-[15vh] flex flex-col items-center justify-center">
         <div className="py-2 px-4 w-[30vw] rounded bg-[#5C76BD] mb-2 flex justify-between">
           <p className="grid place-items-center">All Tasks</p>
-          <p className="py-1 px-4 bg-[#23366D] rounded text-red-500">
+          <p className="py-1 px-5 bg-[#23366D] rounded text-red-500">
             {tasks.length}
           </p>
         </div>
         <div className="py-2 px-4 w-[30vw] rounded bg-[#5C76BD] flex justify-between">
           <p className="grid place-items-center">Completed Tasks</p>
-          <p className="py-1 px-4 bg-[#23366D] rounded text-red-500">
+          <p className="py-1 px-5 bg-[#23366D] rounded text-red-500">
             {tasks.filter((task) => task.isFinished === true).length}
           </p>
         </div>
