ad07f7bbb198fa9bece9923517143b545b2f2bd3
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-18 11:43:37 +0800

    fix: display no tasks if there is no data in db

--- src/app/components/TaskList.tsx
+++ src/app/components/TaskList.tsx
@@ -9,25 +9,11 @@ type TaskListProps = {
 export default function TaskList({ tasks }: TaskListProps) {
   return (
     <div>
-      <ul className="h-[30vh] overflow-y-auto">
+      <ul>
         {tasks.map((task) => (
           <TaskItem key={task.id} tasks={task}></TaskItem>
         ))}
       </ul>
-      <div className=" my-2 h-[15vh] flex flex-col items-center justify-center">
-        <div className="py-2 px-4 w-[30vw] rounded bg-[#5C76BD] mb-2 flex justify-between">
-          <p className="grid place-items-center">All Tasks</p>
-          <p className="py-1 px-5 bg-[#23366D] rounded text-red-500">
-            {tasks.length}
-          </p>
-        </div>
-        <div className="py-2 px-4 w-[30vw] rounded bg-[#5C76BD] flex justify-between">
-          <p className="grid place-items-center">Completed Tasks</p>
-          <p className="py-1 px-5 bg-[#23366D] rounded text-red-500">
-            {tasks.filter((task) => task.isFinished === true).length}
-          </p>
-        </div>
-      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,33 @@ export default async function Home() {
           <h1 className="text-2xl text-white pt-5 text-center">Task Manager</h1>
           <CreateTask />
           <h2 className=" text-white px-4 text-lg">Tasks To Do</h2>
-          {<TaskList tasks={tasks} />}
+          <div className="h-[30vh] overflow-y-auto">
+            {tasks.length > 0 ? (
+              <TaskList tasks={tasks} />
+            ) : (
+              <p className="grid place-items-center text-gray-500 text-xl">
+                No tasks
+              </p>
+            )}
+          </div>
+        </div>
+        <div className=" my-2 h-[15vh] flex flex-col items-center justify-center">
+          <div className="py-2 px-4 w-[30vw] rounded bg-[#5C76BD] mb-2 flex justify-between">
+            <p className="grid place-items-center">All Tasks</p>
+            <p className="py-1 px-5 bg-[#23366D] rounded text-red-500">
+              {tasks.length}
+            </p>
+          </div>
+          <div className="py-2 px-4 w-[30vw] rounded bg-[#5C76BD] flex justify-between">
+            <p className="grid place-items-center">Completed Tasks</p>
+            <p className="py-1 px-5 bg-[#23366D] rounded text-red-500">
+              {
+                tasks.filter(
+                  (tasks: { isFinished: boolean }) => tasks.isFinished === true
+                ).length
+              }
+            </p>
+          </div>
         </div>
       </div>
     </main>
