547687c95b0bd59624f422c0e73759ed6d3c70fe
rjamesmoreno <rendelljames20001@gmail.com>
2024-04-18 13:32:53 +0800

    chore: used shadcn checkbox and tabler edit/delete icons

--- src/app/components/TaskItem.tsx
+++ src/app/components/TaskItem.tsx
@@ -2,6 +2,8 @@ import { ITask } from "@/app/types/tasks";
 import { FinishTask, deleteTask } from "../../../api";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { IconEdit, IconTrash } from "@tabler/icons-react";
+import { Checkbox } from "@/components/ui/checkbox";
 
 type TaskProps = {
   tasks: ITask;
@@ -24,24 +26,23 @@ export default function TaskItem({ tasks }: TaskProps) {
 
   return (
     <div key={tasks.id} className="py-2 px-4 flex">
-      <div className="h-[4vh] w-[2vw] bg-[#23366D] pl-[9px] pt-1 mr-1 rounded-l-lg">
-        <input
-          type="checkbox"
+      <div className="h-[5vh] w-[2vw] bg-[#23366D] grid place-items-center mr-1 rounded-l-lg">
+        <Checkbox
           checked={status}
-          onChange={(e) => {
+          onCheckedChange={(e) => {
             handleFinishTask(tasks.id, !status);
           }}
-        ></input>
+        />
       </div>
-      <div className="bg-[#23366D] w-[24vw] h-[4vh] pl-2 text-white content-center">
+      <div className="bg-[#23366D] w-[24vw] h-[5vh] pl-2 text-white content-center">
         {tasks.title}
       </div>
-      <div className="ml-1">
-        <button
-          onClick={() => handleDeleteTask(tasks.id)}
-          className="bg-red-500 rounded-r-[5px] w-[4vw] h-[4vh]"
-        >
-          Delete
+      <div className="ml-1 bg-[#23366D] rounded-r-[5px] w-[4vw] h-[5vh] flex justify-around">
+        <button>
+          <IconEdit color="white" />
+        </button>
+        <button onClick={() => handleDeleteTask(tasks.id)}>
+          <IconTrash color="red" />
         </button>
       </div>
     </div>
